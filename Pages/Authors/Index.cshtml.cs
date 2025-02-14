2d14e393d126ebc382d32aa0e08935d8409825c1
Andriy Bondarenko <bondareandre@gmail.com>
2025-02-14 20:08:58 +0200

    Sortable melodies and authors

--- Pages/Authors/Index.cshtml.cs
+++ Pages/Authors/Index.cshtml.cs
@@ -9,6 +9,7 @@ using Melodies25.Data;
 using Melodies25.Models;
 using Music;
 using static Music.Messages;
+using Microsoft.Identity.Client;
 
 namespace Melodies25.Pages.Authors
 {
@@ -23,16 +24,40 @@ namespace Melodies25.Pages.Authors
 
         public IList<Author> Author { get;set; } = default!;
         
+        public string SurnameSort { get; set; }
+        public string CountrySort { get; set; }
+        public string MelodiesCountSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
             MessageL(COLORS.yellow, $"AUTHORS/INDEX -  OnGET");
-            Author = await _context.Author.Include(m=>m.Country).ToListAsync();
 
-            foreach(var currentAuthor in Author) {
+            SurnameSort = sortOrder == "surname_asc" ? "surname_desc" : "surname_asc";
+            CountrySort = sortOrder == "country_asc" ? "country_desc" : "country_asc";
+            MelodiesCountSort = sortOrder == "melody_asc" ? "melody_desc" : "melody_asc";
+
+
+            var authorQuery = _context.Author.Include(m => m.Country).AsQueryable();
+
+            foreach (var currentAuthor in authorQuery)
+            {
                 var meloides = await _context.Melody.Where(m => m.AuthorID == currentAuthor.ID).ToListAsync();
                 currentAuthor.MelodiesCount = meloides.Count;
             }
+
+
+            authorQuery = sortOrder switch
+            {
+                "surname_asc" => authorQuery.OrderBy(a => a.Surname == "Українська народна пісня" ? "!" + a.Surname : a.Surname),
+                "surname_desc" => authorQuery.OrderByDescending(a => a.Surname == "Українська народна пісня" ? "!" + a.Surname : a.Surname),
+                "country_asc" => authorQuery.OrderBy(a => a.Country.Name),
+                "country_desc" => authorQuery.OrderByDescending(a => a.Country.Name),
+                "melody_asc" => authorQuery.OrderBy(a => a.MelodiesCount),
+                "melody_desc" => authorQuery.OrderByDescending(a => a.MelodiesCount),
+                _ => authorQuery // Якщо немає сортування, залишаємо список без змін
+            };
+
+            Author = await authorQuery.ToListAsync();           
 
 
         }
--- Pages/Melodies/Index.cshtml.cs
+++ Pages/Melodies/Index.cshtml.cs
@@ -15,6 +15,7 @@ using NAudio.Midi;
 using System.IO;
 using Music;
 using Melody = Melodies25.Models.Melody;
+using Microsoft.Data.SqlClient;
 
 namespace Melodies25.Pages.Melodies
 {
@@ -25,6 +26,10 @@ namespace Melodies25.Pages.Melodies
         public string Msg { get; set; }
 
         public string Errormsg { get; set; }
+
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string CurrentSort { get; set; }
 
 
 
@@ -38,12 +43,26 @@ namespace Melodies25.Pages.Melodies
 
         
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Melody = await _context.Melody
-                .Include(m => m.Author).ToListAsync();
-
             MessageL(COLORS.yellow, $"MELODTY/INDEX -  OnGET");
+
+            TitleSort = sortOrder == "title_asc" ? "title_desc" : "title_asc";
+            AuthorSort = sortOrder == "author_asc" ? "author_desc" : "author_asc";
+            CurrentSort = sortOrder;
+
+            var melodiesQuery = _context.Melody.Include(m => m.Author).AsQueryable();
+
+            melodiesQuery = sortOrder switch
+            {
+                "title_asc" => melodiesQuery.OrderBy(m => m.Title),
+                "title_desc" => melodiesQuery.OrderByDescending(m => m.Title),
+                "author_asc" => melodiesQuery.OrderBy(m => m.Author.Surname).ThenBy(m => m.Author.Name),
+                "author_desc" => melodiesQuery.OrderByDescending(m => m.Author.Name).ThenByDescending(m => m.Author.Surname),
+                _ => melodiesQuery // Якщо немає сортування, залишаємо список без змін
+            };
+
+            Melody = await melodiesQuery.ToListAsync();
 
         }
       
