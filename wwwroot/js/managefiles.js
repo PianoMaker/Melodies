cd5a23743f1d456c6a674c498bd41c5ff943da5f
Andriy Bondarenko <bondareandre@gmail.com>
2025-04-17 00:12:49 +0300

    files page fix

--- wwwroot/js/managefiles.js
+++ wwwroot/js/managefiles.js
@@ -1,108 +1,86 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
     console.log("managefiles.js is running");
-    let midifilesDiv = document.getElementById("midifilesDiv");
-    let midifilesBtn = document.getElementById("midifilesBtn");
-    let showmidiFiles = document.getElementById("showmidiFiles");
-    let midiFilesBox = document.getElementById("midiFilesBox");
+    let midiFilesDiv = document.getElementById("midifilesDiv");
+    let midiFilesBtn = document.getElementById("midifilesBtn");
+    let showmidifiles = document.getElementById("showmidifiles");
+    let midifilesBox = document.getElementById("midifilesBox");
 
-    midifilesDiv.style.display = "none";
-    showmidiFiles.innerText = "показати";
-    midiFilesBox.style.width = "150px";
+    midiFilesDiv.style.display = "none";
+    showmidifiles.innerText = "показати";
+    midifilesBox.style.width = "150px";
 
-    midifilesBtn.addEventListener("click", function () {
-        console.log("managefilesBtn is pressed");
-        midifilesBtn.addEventListener("click", function () {
-            console.log("managefilesBtn is pressed");
-            if (midifilesBtn.innerText === "+") {
-                midifilesBtn.innerText = "-";
-                midifilesDiv.style.display = "block";
-                showmidiFiles.innerText = "сховати";
-                midiFilesBox.style.width = "100%";
-            } else {
-                midifilesBtn.innerText = "+";
-                midifilesDiv.style.display = "none";
-                showmidiFiles.innerText = "показати";
-                midiFilesBox.style.width = "150px";
-            }
-        });
-        
 
-        if (midifilesBtn.innerText = "-") {
-            midifilesBtn.innerText = "+";
-            midifilesDiv.style.display = "none";
+    midiFilesBtn.addEventListener("click", function () {
+        console.log("midifilesBtn is pressed");
+        if (midiFilesBtn.innerText === "+") {
+            midiFilesBtn.innerText = "-";
+            midiFilesDiv.style.display = "block";
+            showmidifiles.innerText = "сховати";
+            midifilesBox.style.width = "100%";
+        } else {
+            midiFilesBtn.innerText = "+";
+            midiFilesDiv.style.display = "none";
+            showmidifiles.innerText = "показати";
+            midifilesBox.style.width = "150px";
         }
-
-    });
-
-    let tempfilesDiv = document.getElementById("tempfilesDiv");
-    let tempfilesBtn = document.getElementById("tempfilesBtn");
-    let showtempFiles = document.getElementById("showtempFiles");
-    let tempFilesBox = document.getElementById("tempFilesBox");
-
-    tempfilesDiv.style.display = "none";
-    showtempFiles.innerText = "показати";
-    tempFilesBox.style.width = "150px";
-
-    tempfilesBtn.addEventListener("click", function () {
-        console.log("managefilesBtn is pressed");
-        tempfilesBtn.addEventListener("click", function () {
-            console.log("managefilesBtn is pressed");
-            if (tempfilesBtn.innerText === "+") {
-                tempfilesBtn.innerText = "-";
-                tempfilesDiv.style.display = "block";
-                showtempFiles.innerText = "сховати";
-                tempFilesBox.style.width = "100%";
-            } else {
-                tempfilesBtn.innerText = "+";
-                tempfilesDiv.style.display = "none";
-                showtempFiles.innerText = "показати";
-                tempFilesBox.style.width = "150px";
-            }
-        });
-
-
-        if (tempfilesBtn.innerText = "-") {
-            tempfilesBtn.innerText = "+";
-            tempfilesDiv.style.display = "none";
-        }
-
     });
 
 
-    let mp3filesDiv = document.getElementById("mp3filesDiv");
-    let mp3filesBtn = document.getElementById("mp3filesBtn");
-    let showmp3Files = document.getElementById("showmp3Files");
-    let mp3FilesBox = document.getElementById("mp3FilesBox");
-
-    mp3filesDiv.style.display = "none";
-    showmp3Files.innerText = "показати";
-    mp3FilesBox.style.width = "150px";
-
-    mp3filesBtn.addEventListener("click", function () {
-        console.log("managefilesBtn is pressed");
-        mp3filesBtn.addEventListener("click", function () {
-            console.log("managefilesBtn is pressed");
-            if (mp3filesBtn.innerText === "+") {
-                mp3filesBtn.innerText = "-";
-                mp3filesDiv.style.display = "block";
-                showmp3Files.innerText = "сховати";
-                mp3FilesBox.style.width = "100%";
-            } else {
-                mp3filesBtn.innerText = "+";
-                mp3filesDiv.style.display = "none";
-                showmp3Files.innerText = "показати";
-                mp3FilesBox.style.width = "150px";
-            }
-        });
-
-
-        if (mp3filesBtn.innerText = "-") {
-            mp3filesBtn.innerText = "+";
-            mp3filesDiv.style.display = "none";
-        }
-
-    });
 
 
 });
+
+
+let mp3filesDiv = document.getElementById("mp3filesDiv");
+let mp3filesBtn = document.getElementById("mp3filesBtn");
+let showmp3files = document.getElementById("showmp3files");
+let mp3filesBox = document.getElementById("mp3filesBox");
+
+mp3filesDiv.style.display = "none";
+showmp3files.innerText = "показати";
+mp3filesBox.style.width = "150px";
+
+mp3filesBtn.addEventListener("click", function () {
+
+    console.log("mp3filesBtn is pressed");
+    if (mp3filesBtn.innerText === "+") {
+        mp3filesBtn.innerText = "-";
+        mp3filesDiv.style.display = "block";
+        showmp3files.innerText = "сховати";
+        mp3filesBox.style.width = "100%";
+    } else {
+        mp3filesBtn.innerText = "+";
+        mp3filesDiv.style.display = "none";
+        showmp3files.innerText = "показати";
+        mp3filesBox.style.width = "150px";
+    }
+});
+
+
+
+
+let tempfilesDiv = document.getElementById("tempfilesDiv");
+let tempfilesBtn = document.getElementById("tempfilesBtn");
+let showtempfiles = document.getElementById("showtempfiles");
+let tempfilesBox = document.getElementById("tempfilesBox");
+
+tempfilesDiv.style.display = "none";
+showtempfiles.innerText = "показати";
+tempfilesBox.style.width = "150px";
+
+tempfilesBtn.addEventListener("click", function () {
+    console.log("tempfilesBtn is pressed");
+        if (tempfilesBtn.innerText === "+") {
+            tempfilesBtn.innerText = "-";
+            tempfilesDiv.style.display = "block";
+            showtempfiles.innerText = "сховати";
+            tempfilesBox.style.width = "100%";
+        } else {
+            tempfilesBtn.innerText = "+";
+            tempfilesDiv.style.display = "none";
+            showtempfiles.innerText = "показати";
+            tempfilesBox.style.width = "150px";
+        }
+ });
+
