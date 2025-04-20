6716db01894560a494ac091de8396ce1aa8bb161
Andriy Bondarenko <bondareandre@gmail.com>
2025-04-21 00:11:46 +0300

    +experimental pages
    (cherry picked from commit a262fa0d0a3effd776c9702e3a525ed6533a9968)
    
    # Conflicts:
    #	Pages/Experimental/Notation.cshtml.cs
    #	Pages/Shared/_adminMenu.cshtml
    #	wwwroot/css/site.css

---
+++ Pages/Experimental/Notation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Melodies25.Pages.Account
+{
+    public class NotationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/notation.js
@@ -0,0 +1,27 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+    const f = new Vex.Flow.Factory({
+        renderer: { elementId: 'notation', width: 600, height: 500 },
+    });
+
+    const score = f.EasyScore();
+    const system = f.System();
+
+    system.addStave({
+        voices: [
+            score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+            score.voice(score.notes('C#4/h, C#4', { stem: 'down' }))
+        ]
+    }).addClef('treble').addTimeSignature('4/4');
+
+    system.addStave({
+        voices: [
+            score.voice(score.notes('C#3/q, B2, A2/16, B2, C#3/8, D3/q', { clef: 'bass', stem: 'up' })),
+            score.voice(score.notes('C#2/h, C#2', { clef: 'bass', stem: 'down' }))
+        ]
+    }).addClef('bass').addTimeSignature('4/4');
+
+    system.addConnector()
+
+    f.draw();
+});
+
