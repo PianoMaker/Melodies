64f9ac851f0ae6fc38e06188d32cc991756224f9
Andriy Bondarenko <bondareandre@gmail.com>
2025-11-23 12:46:18 +0200

    Melody TitleEn Migration

---
+++ Migrations/20251123104004_EnTitle_provided.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Melodies25.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnTitle_provided : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TitleEn",
+                table: "Melody",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleEn",
+                table: "Melody");
+        }
+    }
+}
