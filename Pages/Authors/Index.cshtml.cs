5b4359d76c629f94aee59b3043620cea5d55716f
Andriy Bondarenko <bondareandre@gmail.com>
2025-11-26 21:53:46 +0200

    lng on page create
    (cherry picked from commit f0cfb24af7dcfaf24d4a52318f1514ddb4670d69)

--- Pages/Authors/Index.cshtml.cs
+++ Pages/Authors/Index.cshtml.cs
@@ -11,6 +11,7 @@ using Music;
 using static Music.Messages;
 using Microsoft.Identity.Client;
 using Melodies25.Utilities;
+using Microsoft.AspNetCore.Http;
 
 namespace Melodies25.Pages.Authors
 {
@@ -23,11 +24,19 @@ namespace Melodies25.Pages.Authors
             _context = context;
         }
 
-        public IList<Author> Author { get;set; } = default!;
+        public IList<Author> Author { get; set; } = default!;
+
         
         public string SurnameSort { get; set; }
+        
         public string CountrySort { get; set; }
+        
         public string MelodiesCountSort { get; set; }
+
+        [BindProperty(SupportsGet = true)]        
+        public string SelectedLang { get; set; } = "uk";
+
+
 
         public async Task OnGetAsync(string sortOrder)
         {
@@ -35,48 +44,88 @@ namespace Melodies25.Pages.Authors
             await LoadPage(sortOrder);
         }
 
+        public IActionResult OnPost()
+        {
+            SelectedLang = string.IsNullOrEmpty(SelectedLang) ? "uk" : (SelectedLang == "en" ? "en" : "uk");
+            // PRG: зберігаємо вибір мови в query, щоб він не губився на переходах (у т.ч. сортування)
+            return RedirectToPage(new { SelectedLang = SelectedLang });
+        }
+
         private async Task LoadPage(string sortOrder)
         {
-            
-
-            SurnameSort = sortOrder == "surname_asc" ? "surname_desc" : "surname_asc";
-            CountrySort = sortOrder == "country_asc" ? "country_desc" : "country_asc";
+            // Toggle sort states
+            SurnameSort       = sortOrder == "surname_asc" ? "surname_desc" : "surname_asc";
+            CountrySort       = sortOrder == "country_asc" ? "country_desc" : "country_asc";
             MelodiesCountSort = sortOrder == "melody_asc" ? "melody_desc" : "melody_asc";
 
-            var authorQuery = _context.Author.Include(m => m.Country).AsQueryable();
+            bool isEn    = string.Equals(SelectedLang, "en", StringComparison.OrdinalIgnoreCase);
+            string folkUk = "Українська народна пісня";
+            string folkEn = "Ukrainian folk song";
 
-            // Отримуємо всі мелодії, пов'язані з авторами, за один запит
-            var melodies = await _context.Melody.ToListAsync();
+            // Base query: join with melodies to get count (COUNT done in SQL, no NotMapped sorting issue)
+            var baseQuery =
+                from a in _context.Author.Include(a => a.Country)
+                join m in _context.Melody on a.ID equals m.AuthorID into g
+                select new
+                {
+                    Author = a,
+                    MelodyCount = g.Count()
+                };
 
-            // Збираємо кількість мелодій для кожного автора
-            foreach (var currentAuthor in authorQuery)
+            // Apply sorting
+            baseQuery = sortOrder switch
             {
-                currentAuthor.MelodiesCount = melodies.Count(m => m.AuthorID == currentAuthor.ID);
-            }
+                "surname_asc" => isEn
+                    ? baseQuery.OrderBy(x =>
+                        ((x.Author.SurnameEn ?? x.Author.Surname) == folkEn
+                            ? "!" + (x.Author.SurnameEn ?? x.Author.Surname)
+                            : (x.Author.SurnameEn ?? x.Author.Surname)))
+                    : baseQuery.OrderBy(x =>
+                        (x.Author.Surname == folkUk ? "!" + x.Author.Surname : x.Author.Surname)),
 
-            authorQuery = sortOrder switch
-            {
-                "surname_asc" => authorQuery.OrderBy(a => a.Surname == "Українська народна пісня" ? "!" + a.Surname : a.Surname),
-                "surname_desc" => authorQuery.OrderByDescending(a => a.Surname == "Українська народна пісня" ? "!" + a.Surname : a.Surname),
-                "country_asc" => authorQuery.OrderBy(a => a.Country.Name),
-                "country_desc" => authorQuery.OrderByDescending(a => a.Country.Name),
-                "melody_asc" => authorQuery.OrderBy(a => a.MelodiesCount),
-                "melody_desc" => authorQuery.OrderByDescending(a => a.MelodiesCount),
-                _ => authorQuery // Якщо немає сортування, залишаємо список без змін
+                "surname_desc" => isEn
+                    ? baseQuery.OrderByDescending(x =>
+                        ((x.Author.SurnameEn ?? x.Author.Surname) == folkEn
+                            ? "!" + (x.Author.SurnameEn ?? x.Author.Surname)
+                            : (x.Author.SurnameEn ?? x.Author.Surname)))
+                    : baseQuery.OrderByDescending(x =>
+                        (x.Author.Surname == folkUk ? "!" + x.Author.Surname : x.Author.Surname)),
+
+                "country_asc" => isEn
+                    ? baseQuery.OrderBy(x => x.Author.Country != null ? (x.Author.Country.NameEn ?? "") : "")
+                    : baseQuery.OrderBy(x => x.Author.Country != null ? (x.Author.Country.Name ?? "") : ""),
+
+                "country_desc" => isEn
+                    ? baseQuery.OrderByDescending(x => x.Author.Country != null ? (x.Author.Country.NameEn ?? "") : "")
+                    : baseQuery.OrderByDescending(x => x.Author.Country != null ? (x.Author.Country.Name ?? "") : ""),
+
+                "melody_asc"  => baseQuery.OrderBy(x => x.MelodyCount),
+                "melody_desc" => baseQuery.OrderByDescending(x => x.MelodyCount),
+
+                _ => baseQuery
             };
 
-            Author = await authorQuery.ToListAsync();
+            // Materialize and copy MelodyCount back into NotMapped property
+            var rows = await baseQuery.ToListAsync();
+            foreach (var row in rows)
+            {
+                row.Author.MelodiesCount = row.MelodyCount; // safe: NotMapped
+            }
+
+            Author = rows.Select(r => r.Author).ToList();
         }
 
         public async Task OnPostTranslitAsync(string sortOrder)
         {
             MessageL(COLORS.yellow, "AUTHOR/ONPOSTTRANSLIT");
 
+            SelectedLang = Request.Cookies["site_lang"] ?? SelectedLang ?? "uk";
+
             await LoadPage(sortOrder);
 
             foreach (var author in Author)
             {
-                if(string.IsNullOrEmpty(author.NameEn) && !string.IsNullOrEmpty(author.Name))
+                if (string.IsNullOrEmpty(author.NameEn) && !string.IsNullOrEmpty(author.Name))
                 {
                     var nameEn = Translit.Transliterate(author.Name);
                     author.NameEn = nameEn;
