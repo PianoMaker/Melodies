5af15246239fd8e412c011207ba9be915c419975
Andriy Bondarenko <bondareandre@gmail.com>
2025-12-17 19:22:03 +0200

    correct height*width for mobile

--- wwwroot/lib/midirender/midiRenderer.js
+++ wwwroot/lib/midirender/midiRenderer.js
@@ -463,7 +463,7 @@ function renderMeasuresToNotation(startAtMeasureIndex, measures, maxBarsToRender
 
 function getRenderDimensions(effectiveWidth, scaleFactor, rowsHeight, containerWidth) {
 	const rendererWidth = Math.max(Math.round(effectiveWidth / scaleFactor), 200);
-	const rendererHeight = Math.max(Math.round(rowsHeight * scaleFactor ), 100);
+	const rendererHeight = Math.max(Math.round(rowsHeight * scaleFactor * scaleFactor ), 100);
 	console.log(`renderMidiFileToNotation: rowsHeight = ${rowsHeight}, containerWidth=${containerWidth}`);
 	console.log(`renderMidiFileToNotation: rendererHight = ${rowsHeight}, rendererWidth=${containerWidth}`);
 	return { rendererWidth, rendererHeight };
