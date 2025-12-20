c9b5370eb839374ac681deac5ccb39eecec45c9c
Andriy Bondarenko <bondareandre@gmail.com>
2025-12-20 19:05:23 +0200

    design changes - colors, attempts width notesinfo
    (cherry picked from commit 3fa8f6970cd875b3f75901e8e06982e715bcbba0)
    
    # Conflicts:
    #	wwwroot/css/notes.css
    #	wwwroot/css/site.css

---
+++ Pages/Admin/Colors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Melodies25.Pages.Admin
+{
+    public class ColorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
