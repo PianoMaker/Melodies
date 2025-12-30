9003a6fbb06a9de793eb9cc9e80232e9225f6495
Andriy Bondarenko <bondareandre@gmail.com>
2025-12-30 11:45:12 +0100

    Add Country flag URL support and preview in Edit page
    (cherry picked from commit df1849bb4746d78260c7e700c20760d68ac0a3fc)

---
+++ Migrations/20251230103610_CountryFlag.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Melodies25.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountryFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlagUrl",
+                table: "Country",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlagUrl",
+                table: "Country");
+        }
+    }
+}
